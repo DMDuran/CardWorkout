a4b472d2f3688713065fd65f2620a918b5ca3154
DMDuran <92621818+DMDuran@users.noreply.github.com>
2022-11-10 17:05:47 -0600

    Creating a swift file called Deck that will be a struct and call all UIImages from assets Added features in VC that will perform a stop when the stopButton is pressed and a restart action when the restartButton is pressed. Added Deck function to the VC to allow UIImage shuffling for the cards.  Added a viewWillDisappear to allow timer to start when not using to help with memory management.

--- CardWorkout/CardSelectionVC.swift
+++ CardWorkout/CardSelectionVC.swift
@@ -15,33 +15,54 @@ class CardSelectionVC: UIViewController {
     @IBOutlet var cardImageView: UIImageView!
     //Grabbing all buttons in storyboard that way we can customize them in one go
     @IBOutlet var buttons: [UIButton]!
+    //Creating an array so we can have Images shuffle
+    var cards: [UIImage] = Deck.allValues
+    
     
     //Going to force unwrap this timer becuase we are not going to be relying on any other data for this timer
     var timer: Timer!
     
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        startTimer()
         //Running this loop for create a rounded couner for all buttons tied to (@IBOutlet var buttons: [UIButton]!)
         for button in buttons {
             button.layer.cornerRadius = 8
         }
-        
+    }
+    
+    
+    //This will invalidate the timer when you leave the screen or go to a different one
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        timer.invalidate()
     }
 
-    func startTime() {
-        
+    
+    //Function that will be shuffling images every 0.5 seconds.
+    func startTimer() {
+        timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(showRandomImage), userInfo: nil,
+        repeats: true)
     }
+    
+    
+    @objc func showRandomImage() {
+        //Creating a default face for the optional.  This is going to be the ace card.
+        cardImageView.image = cards.randomElement() ?? UIImage(named: "AS")
+    }
+    
     
     //Creating Actions (When you want something to be done.)
     
     @IBAction func stopButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
     }
     
     @IBAction func restartButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
+        startTimer()
     }
-
-    @IBAction func rulesButtonTapped(_ sender: UIButton) {
-    }
+    
     
 }
